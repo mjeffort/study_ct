2c399c6c8ddcc0b8b8a4cdeaa4d6d9bf4fc6825d
myeongjjun <mjk3026@gmail.com>
2022-05-21 14:44:24 +0900

    hr_hourglassSum

---
+++ src/hackerrank/array2d/Solution.java
@@ -0,0 +1,73 @@
+package hackerrank.array2d;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        // Write your code here
+        int[] dx = {1, 1, 1, -1, -1 , -1};
+        int[] dy = {-1, 0, 1, -1, 0, 1};
+
+        int res = Integer.MIN_VALUE;
+
+        for (int i = 1; i < 5; i++) {
+            for (int j = 1; j < 5; j++) {
+                int sum = arr.get(i).get(j);
+                for (int k = 0; k < 6; k++) {
+                    sum += arr.get(i + dx[k]).get(j + dy[k]);
+                }
+                res = Math.max(res, sum);
+            }
+
+        }
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
