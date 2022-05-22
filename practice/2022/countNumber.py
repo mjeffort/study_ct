26f77adf053677ca83f468f541e15b7e56cb7526
myeongjjun <mjk3026@gmail.com>
2022-05-22 16:50:54 +0900

    20220521

---
+++ practice/2022/countNumber.py
@@ -0,0 +1,17 @@
+#-- encoding: utf-8 --
+from bisect import bisect_left, bisect_right
+
+def count_by_range(array, left_value, right_value):
+    right_value = bisect_right(array, right_value)
+    left_value = bisect_left(array, left_value)
+    return right_value -  left_value
+
+n, x = map(int ,input().split())
+array = list(map(int, input().split()))
+
+count =count_by_range(array, x, x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)
